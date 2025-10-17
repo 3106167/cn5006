1036e99f69ecc45eae567bce9d1163e9eb7996ce
3106167 <u3106167@uel.ac.uk>
2025-10-17 11:52:41 +0000

    week 3 lap tasks

--- week3/index.js
+++ week3/index.js
@@ -1,1 +1,51 @@
+function EmployeeInfo (name,age)
 
+{
+    console.log ("welcome"+ name + "your age is" + age)
+}
+
+name= "Kajana"
+age= "22"
+
+// defination of the function EmployeeInfo 
+function EmployeeInfo(name,age) 
+{ 
+console.log("Wellcome " + name+ "Your age is "+ age) 
+} 
+console.log ("This is my name age details") 
+name="Kajana" 
+age= "22"
+// calling of the function EmployeeInfo 
+EmployeeInfo(name,age)
+
+const EmpSkills= (skills)=> { 
+  console.log("Expert in "+ skills) 
+} 
+EmpSkills("java") 
+
+const student= require('./studentinfo') 
+const person = require('./person')  
+// because getName is the function so we use () 
+console.log("Student Name:" +student.getName())  
+console.log(student.Location())  
+console.log(student.dob)  
+// because dob is a variable so we do nt use () 
+console.log(student.Studentgrade()) 
+console.log("grade is "+student.Studentgrade(55) ) 
+// creating new Person 
+person1= new person("Kajana","UEL Campus","myemail@gmail.com") 
+console.log("using Person Module",person1.getPersonInfo()) 
+console.log("Programe ended") 
+
+os=require("os") 
+const util=require('util') 
+console.log("temporary directory"+ os.tmpdir() ) 
+console.log("hostname: "+ os.hostname()) 
+console.log("OS : " + os.platform() +"release:"+ os.release()) 
+console.log("Uptime"+ (os.uptime())/3600 +" hours") 
+console.log("userInfo" + util.inspect(os.userInfo())) 
+console.log("Memory "+ os.totalmem()/1000000000 + "Giga byte") 
+console.log(" free: "+os.freemem()/1000000000 + "Giga byte") 
+console.log("CPU "+ util.inspect(os.cpus())) 
+console.log("Network"+ util.inspect(os.networkInterfaces())) 
+console.log("programe end") 
