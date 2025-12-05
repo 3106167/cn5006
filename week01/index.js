b11f3267024aefd7c1963dbbba1c4a68c3c52131
3106167 <u3106167@uel.ac.uk>
2025-12-05 15:05:25 +0000

    portfolio task

--- week01/index.js
+++ week01/index.js
@@ -1,5 +1,16 @@
-console.log("This is my first programme")
-console.log("well come John your month salary is 500000")
+console.log("this is my first programe")
+console.log("wellcome john your monthly salary is 500000")
+
+
+const num1 = 5;
+const num2 = 3;
+//add two number
+const sum = num1 + num2;
+//display the sum
+console.log('the sum of' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+
+
 // app.js or index.js
 const prompt = require('prompt-sync')(); // This line is essential
 console.log("starting")
@@ -20,3 +31,4 @@ console.log("The number is zero");
 else {
 console.log("The number is negative");
 }
+
